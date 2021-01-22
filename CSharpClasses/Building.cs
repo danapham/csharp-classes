299475b66674fbd2ece182559bc3a55be23356fd
Dana Pham <danapham12@gmail.com>
2021-01-21 23:16:54 -0600

    saving

--- CSharpClasses/Building.cs
+++ CSharpClasses/Building.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace CSharpClasses
 {
-    class Building
+    public class Building
     {
+        private string _designer { get; set; }
+        private DateTime _dateConstructed { get; set; }
+        private string _address { get; set; }
+        private string _owner { get; set; }
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume => Width * Depth * 3;
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct() =>_dateConstructed = DateTime.Now;
+        public void Purchase(string name) => _owner = name;
+
     }
 }
